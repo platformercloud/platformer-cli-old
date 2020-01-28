96edd5b6b7757ac1acface22283c4d0da568342e
Chamod Perera <chamod.p@platformer.com>
2020-01-28 08:31:54 +0530

    Platformer init banner done

--- cmd/convert.go
+++ cmd/convert.go
@@ -17,7 +17,6 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -32,20 +31,15 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("convert called")
+		switch args[0] {
+		case "kompose":
+			fmt.Println("Looking for docker-compose.yml....")
+			//exec.Cmd{kompose}
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(convertCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// convertCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// convertCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/init.go
+++ cmd/init.go
@@ -43,7 +43,7 @@ to quickly create a Cobra application.`,
 #        #        #     #     #     #        #     #  #    #   #     #  #        #    #  
 #        #######  #     #     #     #        #######  #     #  #     #  #######  #     #`
 		fmt.Println(banner)
-		fmt.Println("\nYou're about to initialize a Firebase project in this directory: ")
+		fmt.Println("\nYou're about to initialize a Platformer project in this directory: ")
 		dir, err := os.Getwd()
 		if err != nil {
 			log.Fatal(err)
@@ -54,14 +54,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(initCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
