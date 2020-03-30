b4b46c0b06bb1f960528219b2dfd2c1a8b1e4da5
Chamod Shehanka <hcsperera@outlook.com>
2020-03-30 16:28:54 +0530

    Update

--- cmd/login.go
+++ cmd/login.go
@@ -16,9 +16,10 @@ limitations under the License.
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
-	"os/exec"
+	"os"
 )
 
 // loginCmd represents the login command
@@ -31,18 +32,26 @@ and usage of using your command. For example:`,
 		fmt.Println("Allow Platformer to collect anonymous CLI usage and error reporting information?")
 		fmt.Println("\nVisit this URL on any device to login in:")
 
-		// let's try the pwd command here
-		var out, err = exec.Command("firebase login").Output()
-		if err != nil {
-			fmt.Printf("%s", err)
-		}
-		fmt.Println("Command Successfully Executed")
-		var output = string(out[:])
-		fmt.Println(output)
+		var loginURL = `https://accounts.google.com/o/oauth2/auth?client_id=563584335869-fgrhgmd47bqnekij5i8b5pr03ho849e6.apps.googleusercontent.com&scope=email%20openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloudplatformprojects.readonly%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Ffirebase%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform&response_type=code&state=883124016&redirect_uri=http%3A%2F%2Flocalhost%3A9005`
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println(loginURL)
+		response, _ := reader.ReadString('\n')
+		fmt.Println(response)
+
+
+
+		//var out, err = exec.Command(loginURL).Output()
+		//if err != nil {
+		//	fmt.Printf("%s", err)
+		//}
+		//fmt.Println("Command Successfully Executed")
+		//var output = string(out[:])
+		//fmt.Println(output)
+
 	},
 }
 
-func execute(){
+func execute() {
 
 }
 
@@ -52,7 +61,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
+	// and all sub commands, e.g.:
 	// loginCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
