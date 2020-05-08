5cc46392bce49374cd6ebfab2e82ce9361175f01
Binura Gunasekara <binura.g@platformer.com>
2020-05-08 13:12:17 +0000

    Feature/replace promptui with survey/v2

--- cmd/selectprompt/project.go
+++ cmd/selectprompt/project.go
@@ -3,8 +3,8 @@ package selectprompt
 import (
 	"fmt"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/fatih/color"
-	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 	"gitlab.platformer.com/project-x/platformer-cli/internal/auth"
 	"gitlab.platformer.com/project-x/platformer-cli/internal/cli"
@@ -35,24 +35,24 @@ func selectProjectWithPrompt() error {
 		return err
 	}
 
-	prompt := promptui.Select{
-		Label: "Select Project from " + orgName,
-		Items: projectList.Names(),
+	prompt := &survey.Select{
+		Message: "Select Organization",
+		Options: projectList.Names(),
 	}
 
-	_, selectedProjectName, err := prompt.Run()
-	if err != nil {
-		// Do nothing; this error is thrown if the user quits the CLI with ctrl+C
+	var selectedProjectName string
+	if err := survey.AskOne(prompt, &selectedProjectName); err != nil {
+		return cli.TransformSurveyError(err)
 	}
 
 	if selectedProjectName == "" {
-		return &cli.UserError{Message: "no project has been selected"}
+		return &cli.UserError{Message: "no organization has been selected"}
 	}
 
 	config.SetDefaultProject(selectedProjectName)
 
 	green := color.New(color.FgHiGreen).SprintfFunc()
-	fmt.Printf("%s > %s has been set as your default organization and project\n",
+	fmt.Printf("%s and %s has been set as your default Organization and Project\n",
 		green(orgName),
 		green(selectedProjectName),
 	)
--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
-	github.com/manifoldco/promptui v0.7.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/mapstructure v1.3.0 // indirect
 	github.com/pelletier/go-toml v1.7.0 // indirect
