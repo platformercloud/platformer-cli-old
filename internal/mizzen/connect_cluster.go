03b060cc122a20a524027f1f902f34ec19a49024
Binura Gunasekara <binura.g@platformer.com>
2020-06-28 10:40:39 +0000

    Ironed out some bugs in cluster connect Better error logging for failed requests

--- internal/mizzen/connect_cluster.go
+++ internal/mizzen/connect_cluster.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"gitlab.platformer.com/project-x/platformer-cli/internal/cli"
@@ -27,13 +28,16 @@ type credentials struct {
 func ConnectAndInstallAgent(kw *KubectlWrapper, orgID string, projectID string, clusterName string) error {
 	credentials, err := register(orgID, projectID, clusterName)
 	if err != nil {
+		if _, ok := err.(*cli.UserError); ok {
+			return err
+		}
 		return &cli.InternalError{
 			Message: "Failed to register cluster",
 			Err:     err,
 		}
 	}
 
-	if err := installAgent(kw, credentials); err != nil {
+	if err := installAgent(kw, clusterName, credentials); err != nil {
 		return &cli.InternalError{
 			Message: "Failed to install in-cluster agent. Please check if kubectl has access to the requested cluster",
 			Err:     err,
@@ -46,13 +50,11 @@ func ConnectAndInstallAgent(kw *KubectlWrapper, orgID string, projectID string, 
 func register(orgID string, projectID string, clusterName string) (*credentials, error) {
 	var body bytes.Buffer
 	json.NewEncoder(&body).Encode(struct {
-		ClusterType    string   `json:"cluster_type"`
 		ClusterName    string   `json:"cluster_name"`
 		ProjectID      string   `json:"project_id"`
 		OrganizationID string   `json:"organization_id"`
 		WhitelistIPs   []string `json:"whitelist_ips"`
 	}{
-		"private",
 		clusterName,
 		projectID,
 		orgID,
@@ -74,7 +76,11 @@ func register(orgID string, projectID string, clusterName string) (*credentials,
 	}
 
 	if r.StatusCode >= 400 {
-		return nil, fmt.Errorf("bad request error: %s", string(b))
+		errMsg := string(b)
+		if strings.Contains(errMsg, "already exists") {
+			return nil, &cli.UserError{Message: "A Cluster with the same name is already registered under the selected Project"}
+		}
+		return nil, fmt.Errorf("bad request error: %s", errMsg)
 	}
 
 	var creds credentials
@@ -85,7 +91,7 @@ func register(orgID string, projectID string, clusterName string) (*credentials,
 	return &creds, nil
 }
 
-func installAgent(kw *KubectlWrapper, creds *credentials) error {
+func installAgent(kw *KubectlWrapper, clusterName string, creds *credentials) error {
 	encodedToken := base64.StdEncoding.EncodeToString([]byte(creds.ClientID + ";" + creds.ClientSecret))
 	client := &http.Client{
 		Timeout: time.Second * 30,
@@ -98,10 +104,10 @@ func installAgent(kw *KubectlWrapper, creds *credentials) error {
 
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		return err
+		return fmt.Errorf("cannot read request body: %w", err)
 	}
 
-	if _, err := kw.cmdWithStdinPiped(bytes.NewBuffer(b), "apply", "-f", "-"); err != nil {
+	if _, err := kw.cmdWithStdinPiped(bytes.NewBuffer(b), "--cluster", clusterName, "apply", "-f", "-"); err != nil {
 		return fmt.Errorf("failed to install agent: %w", err)
 	}
 
