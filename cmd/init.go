43e3cc3abc18f8703f01849233bfc6590c2f5974
Binura Gunasekara <binura.g@gmail.com>
2020-08-14 10:17:51 +1000

    Added support for windows ANSI color rendering

--- cmd/init.go
+++ cmd/init.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/windows"
 )
 
 // initCmd represents the init command
@@ -30,5 +32,14 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
+	enableWindowsColorSupport()
 	rootCmd.AddCommand(initCmd)
+}
+
+// Cross-platformer compatible, enables ANSI colors on windows terminals.
+func enableWindowsColorSupport() {
+	stdout := windows.Handle(os.Stdout.Fd())
+	var originalMode uint32
+	windows.GetConsoleMode(stdout, &originalMode)
+	windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 }
--- go.mod
+++ go.mod
@@ -25,7 +25,7 @@ require (
 	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
 	golang.org/x/net v0.0.0-20191004110552-13f9640d40b9 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/appengine v1.5.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
